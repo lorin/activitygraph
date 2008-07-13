f1a6d642f8455566f74bb33067e06b551219e8fb
lorinh@gmail.com <lorinh@gmail.com@2e9e74aa-6f4c-0410-9ed1-357f7112cff0>
2008-07-13 21:08:43 +0000

    Updated the svn test to reflect the ActivityGraph project migration.

--- trunk/test/edu/unl/cse/activitygraph/SvnDataSourceTest.java
+++ trunk/test/edu/unl/cse/activitygraph/SvnDataSourceTest.java
@@ -11,14 +11,20 @@ import edu.unl.cse.activitygraph.sources.SVNDataSource;
 public class SvnDataSourceTest {
 	@Test
 	public void testIsNonEmpty() throws SVNException {
-		SVNDataSource svnds = new SVNDataSource("http://cse.unl.edu:8080/svn/ActivityGraph");
+		SVNDataSource svnds = new SVNDataSource("http://activitygraph.googlecode.com/svn/");
 		assertFalse(svnds.isEmpty());
 	}
 	
+	
+	// This test is currently commented out because we don't have a publicly accessible 
+	// empty svn directory. You can create your own by doing "svnadmin creaate /tmp/tester"
+	// and then uncomment the code below, if you like.
+	/*
 	@Test
 	public void testIsEmpty() throws SVNException {
-		SVNDataSource svnds = new SVNDataSource("http://cse.unl.edu:8080/svn/empty");
+		SVNDataSource svnds = new SVNDataSource("file:///tmp/tester");
 		assertTrue(svnds.isEmpty());
 	}
+	*/
 
 }
